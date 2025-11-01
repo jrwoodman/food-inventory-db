f4c1bc6de864f8f425b7806a4e9bda411c52085d
Food Inventory Developer <developer@foodinventory.local>
2025-11-01 12:29:37 -0500

    Fix blank alert boxes in edit user view
    - Prevents empty alert boxes from displaying when variables are empty strings

--- src/views/users/edit.php
+++ src/views/users/edit.php
@@ -45,11 +45,11 @@
     <div class="container">
         <h1>Edit User</h1>
 
-        <?php if(isset($error)): ?>
+        <?php if(!empty($error)): ?>
             <div class="alert alert-danger"><?php echo htmlspecialchars($error); ?></div>
         <?php endif; ?>
 
-        <?php if(isset($success)): ?>
+        <?php if(!empty($success)): ?>
             <div class="alert alert-success"><?php echo htmlspecialchars($success); ?></div>
         <?php endif; ?>
 
